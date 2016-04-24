3ecdd3a83214bce40d3fe40ccb0565300f49f4b8
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2016-04-24 14:02:58 +0100

    Add deps

---
+++ vendor/github.com/anacrolix/utp/deadlines.go
@@ -0,0 +1,57 @@
+package utp
+
+import "time"
+
+type deadline struct {
+	t      time.Time
+	passed bool
+	timer  *time.Timer
+}
+
+func (me *deadline) set(t time.Time) {
+	me.passed = false
+	me.t = t
+	me.timer = time.AfterFunc(0, me.callback)
+}
+
+func (me *deadline) callback() {
+	mu.Lock()
+	defer mu.Unlock()
+	if me.t.IsZero() {
+		return
+	}
+	if time.Now().Before(me.t) {
+		me.timer.Reset(me.t.Sub(time.Now()))
+		return
+	}
+	me.passed = true
+	cond.Broadcast()
+}
+
+// This is embedded in Conn to provide deadline methods for net.Conn. It
+// tickles global mu and cond as required.
+type connDeadlines struct {
+	read, write deadline
+}
+
+func (c *connDeadlines) SetDeadline(t time.Time) error {
+	mu.Lock()
+	defer mu.Unlock()
+	c.read.set(t)
+	c.write.set(t)
+	return nil
+}
+
+func (c *connDeadlines) SetReadDeadline(t time.Time) error {
+	mu.Lock()
+	defer mu.Unlock()
+	c.read.set(t)
+	return nil
+}
+
+func (c *connDeadlines) SetWriteDeadline(t time.Time) error {
+	mu.Lock()
+	defer mu.Unlock()
+	c.write.set(t)
+	return nil
+}
